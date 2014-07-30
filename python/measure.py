8fb703e6bb123ca635dbd6719ea77ab99d316aef
FratNaji <fratnaji@ML-NALNAJI-OSX-SF.local>
2014-07-30 13:08:36 -0700

    removed print and sleep

--- python/measure.py
+++ python/measure.py
@@ -5,14 +5,14 @@ GPIO.setmode(GPIO.BCM)
 TRIG = 23
 ECHO = 24
  
-print "Distance Measurement In Progress"
+
  
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
  
 GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(2)
+
+time.sleep(.2)
  
 GPIO.output(TRIG, True)
 time.sleep(0.00001)
