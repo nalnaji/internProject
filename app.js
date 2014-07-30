dc15dd76af45db4fd1f9d2775a9b2389e87d1c47
FratNaji <fratnaji@ML-NALNAJI-OSX-SF.local>
2014-07-29 17:14:39 -0700

    adding range sensor code

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 var express = require('express');
 var app = express();
-
-
+var gpio = require("pi-gpio");
+var sleep = require('sleep');
 app.listen(8080);
 
-app.get('/test', function (req, res) {
-    res.send(null);
+app.get('/ranger_sensor', function (req, res) {
+    var TRIG = 23;
+    var ECHO = 24;
+    var start;
+    var end ;
+    gpio.setDirection(TRIG, 'out', function(){
+      gpio.setDirection(ECHO, 'in', function(){
+        gpio.write(TRIG, 0, function(){
+          sleep.sleep(2);
+
+          gpio.write(TRIG, 1, function(){
+            sleep.sleep(0.00001);
+            gpio.write(TRIG, 0, function(){
+              gpio.read(ECHO, function(err, value){
+
+                while(value == 0){
+                  start = Date.now();
+                }
+
+                while(value == 1){
+                  end = Date.now();
+                }
+                var duration = end.getTime() - start.getTime();
+                var distance = Math.round(duration * 17150);
+
+                res.send('DISTANCE: ' + distance);
+
+              });
+            });
+          });
+        });
+      });
+    });
+
 });
 
 
