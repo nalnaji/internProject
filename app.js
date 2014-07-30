707f9a12df5880058b8537532aa014d5eb7ee0aa
FratNaji <fratnaji@ML-NALNAJI-OSX-SF.local>
2014-07-29 18:26:14 -0700

    hardcoding bitches

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/ranger_sensor', function (req, res) {
             gpio.write(TRIG, 0, function(err){
               if(err) throw err;
               var pathToECHO = '/sys/devices/virtual/gpio/gpio24/value';
-              console.log(fs.readFileSync(pathToECHO));
+              console.log(fs.readFileSync(pathToECHO, {encoding: 'utf8'}));
               while(fs.readFileSync(pathToECHO) == '0' ){
                 start = Date.now();
               }
