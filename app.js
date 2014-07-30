31271ee328fc6ea5f1416e286e534e70e0be7c78
FratNaji <fratnaji@ML-NALNAJI-OSX-SF.local>
2014-07-30 11:59:51 -0700

    playing with python

--- app.js
+++ app.js
@@ -3,57 +3,14 @@ var app = express();
 var gpio = require("pi-gpio");
 var sleep = require('sleep');
 var fs = require('fs');
+var PythonShell = require('python-shell');
 app.listen(8080);
 
 app.get('/ranger_sensor', function (req, res) {
-    var TRIG = 16;
-    var ECHO = 18;
-    var start;
-    var end ;
-
-    gpio.open(TRIG, 'out', function(err) {
-      if(err) throw err;
-      gpio.open(ECHO, 'in', function(err) {
-        if(err) throw err;
-        gpio.write(TRIG, 0, function(err){
-          if(err) throw err;
-          console.log("Waiting For Sensor To Settle");
-          sleep.sleep(2);
-
-          gpio.write(TRIG, 1, function(err){
-            if(err) throw err;
-            sleep.usleep(1000);
-            gpio.write(TRIG, 0, function(err){
-              if(err) throw err;
-              var pathToECHO = '/sys/devices/virtual/gpio/gpio24/value';
-              console.log(fs.readFileSync(pathToECHO, {encoding: 'utf8'}).charAt(0));
-              start = Date.now();
-              while(fs.readFileSync(pathToECHO, {encoding: 'utf8'}).charAt(0) == '0' ){
-                start = Date.now();
-              }
-              console.log('Exited first while: ' + start);
-              //console.log(fs.readFileSync(pathToECHO, {encoding: 'utf8'}));
-              while(fs.readFileSync(pathToECHO, {encoding: 'utf8'}).charAt(0) == '1'){
-                end = Date.now();
-              }
-              console.log('Exited second while: '+ end);
-              console.log(start +'');
-              console.log(end +'');
-              var duration = end - start;
-
-              var distance = Math.round(duration * 17150);
-
-              gpio.close(TRIG, function(err){
-                gpio.close(ECHO, function(err){
-                  res.send('DISTANCE: ' + distance);
-                });
-              });
-            });
-          });
-        });
-      });
+    PythonShell.run('measure.py', function (err, results) {
+      console.log('' + results);
+      res.send(null);
     });
-
 });
 
 app.get('/togglelight', function (req, res) {
